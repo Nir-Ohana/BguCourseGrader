5720cd0b75d89bad14b54fba0ebacace2e3eef87
omernutovits <gunrbuyuch.5>
2021-05-28 14:30:50 +0300

    Contact Us page+form, trying to connect to firebase, currently unsuccessfu;

--- lib/models/appBar.dart
+++ lib/models/appBar.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:bgu_course_grader/screens/settings/settings_main.dart';
 import 'package:provider/provider.dart';
 
-
-
+import 'inputDec.dart';
 
 
 
@@ -15,6 +14,15 @@ import 'package:provider/provider.dart';
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context) {
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: SettingsForm(),
+        );
+      });
+    }
+
     final authBloc = Provider.of<AuthBloc>(context);
     return AppBar(
         backgroundColor: Colors.orange[800],
@@ -40,7 +48,9 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
               style: ButtonStyle(
                 backgroundColor:  MaterialStateProperty.all<Color>(Colors.orange[800]),
               ),
-              onPressed:  () => Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsMenu())),
+              onPressed:  () => Scaffold(
+                drawer: SettingsMenu()
+              ),
               icon: Icon(Icons.settings),
               label: Text('הגדרות')
           ),
@@ -52,5 +62,81 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => new Size.fromHeight(AppBar().preferredSize.height);
 }
+
+
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+  final List<int> strengths = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+  // form values
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Update your brew settings.',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            decoration: textInputDecoration,
+            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+            onChanged: (val) => setState(() => _currentName = val),
+          ),
+          SizedBox(height: 10.0),
+          DropdownButtonFormField(
+            value: _currentSugars ?? '0',
+            decoration: textInputDecoration,
+            items: sugars.map((sugar) {
+              return DropdownMenuItem(
+                value: sugar,
+                child: Text('$sugar sugars'),
+              );
+            }).toList(),
+            onChanged: (val) => setState(() => _currentSugars = val ),
+          ),
+          SizedBox(height: 10.0),
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min: 100.0,
+            max: 900.0,
+            divisions: 8,
+            onChanged: (val) => setState(() => _currentStrength = val.round()),
+          ),
+          RaisedButton(
+              color: Colors.pink[400],
+              child: Text(
+                'Update',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () async {
+                print(_currentName);
+                print(_currentSugars);
+                print(_currentStrength);
+              }
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 
--- lib/screens/contact_us.dart
+++ lib/screens/contact_us.dart
@@ -1,13 +1,113 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:bgu_course_grader/models/appBar.dart';
 
-class ContactUs extends StatefulWidget {
-  @override
-  _ContactUsState createState() => _ContactUsState();
-}
-
-class _ContactUsState extends State<ContactUs> {
+class ContactUs extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Text('contact us');
-  }
-}
+    final appTitle = 'Contact Us';
+    return MaterialApp(
+    title: appTitle,
+    home: Scaffold(
+    appBar: MyAppBar(),
+    body: MyCustomForm(),
+    ),
+    );
+    }
+    }
+// Create a Form widget.
+    class MyCustomForm extends StatefulWidget {
+    @override
+    MyCustomFormState createState() {
+    return MyCustomFormState();
+    }
+    }
+// Create a corresponding State class. This class holds data related to the form.
+    class MyCustomFormState extends State<MyCustomForm> {
+      final nameController = TextEditingController();
+      final messageController = TextEditingController();
+      // Create a global key that uniquely identifies the Form widget
+      // and allows validation of the form.
+      final _formKey = GlobalKey<FormState>();
+      @override
+      void dispose() {
+        // Clean up the controller when the widget is disposed.
+        nameController.dispose();
+        messageController.dispose();
+        super.dispose();
+      }
+
+      @override
+      Widget build(BuildContext context) {
+        String name;
+        String message;
+        final FirebaseFirestore firestore = FirebaseFirestore.instance;
+        // Build a Form widget using the _formKey created above.
+        return Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                controller: nameController,
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.person),
+                  hintText: 'Enter your name',
+                  labelText: 'Name',
+
+                ),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                controller: messageController,
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.question_answer),
+                  hintText: 'How may we help you?',
+                  labelText: 'Message',
+                ),
+                maxLines: null,
+              ),
+              new Container(
+                padding: const EdgeInsets.only(left: 150.0, top: 40.0),
+                child: new ElevatedButton(
+
+        onPressed: () {
+          FocusScope.of(context).unfocus();
+        // Validate returns true if the form is valid, or false otherwise.
+        if (_formKey.currentState.validate()) {
+        // If the form is valid, display a snackbar. In the real world,
+        // you'd often call a server or save the information in a database.
+        ScaffoldMessenger.of(context)
+            .showSnackBar(
+        SnackBar(content: Text('Processing Data')));
+        () async {
+          await firestore.collection("ContactUs").add({
+            'name': nameController.text,
+            'message': messageController.text
+          });
+          Navigator.of(context).popUntil((route) => route.isFirst);
+        };
+        }
+        },
+                  child: const Text('Submit')
+
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    }
