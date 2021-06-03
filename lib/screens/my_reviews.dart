be7e864adf646e2f5e5c6a4e945b6dfb00a506cc
Nir <speednir123@gmail.com>
2021-06-03 18:42:46 +0300

    Ya walli x 3

--- lib/screens/my_reviews.dart
+++ lib/screens/my_reviews.dart
@@ -1,3 +1,6 @@
+import 'package:bgu_course_grader/screens/loading.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MyReviews extends StatefulWidget {
@@ -6,8 +9,53 @@ class MyReviews extends StatefulWidget {
 }
 
 class _MyReviewsState extends State<MyReviews> {
+
+  List<Container> sup = [];
+  List<Container> sup2 = [];
+
+  Future<List<Widget>> getReviewsByUserEmail() async
+  {
+    final user = FirebaseAuth.instance.currentUser;
+    var poop = await FirebaseFirestore.instance.collection('Reviews').where('email', isEqualTo: user.email);
+
+    poop.get()
+        .then((value) => {
+      value.docs.forEach((element) {
+        sup.add(Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: Center(child: Text(element['review_content'])),
+        )
+            );
+      })
+    });
+    print(sup);
+    sup2 = sup; // TODO: change it asap
+    sup=[];
+    return sup2;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('my reviews');
+
+
+    return FutureBuilder(
+      future: getReviewsByUserEmail(),
+    builder: (BuildContext context, AsyncSnapshot<List<Widget>> snapshot){
+    if(snapshot.hasData)
+    return Scaffold(
+      body: ListView(
+      padding: const EdgeInsets.all(8),
+      children:
+      sup2
+      ),
+      backgroundColor: Colors.orange[800],
+      );
+    return Loading();});
+
+
+
+
+
   }
 }
