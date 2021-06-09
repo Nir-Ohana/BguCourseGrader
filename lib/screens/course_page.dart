27cc3209ae54b383d22b32d02c2229a5fa47c658
omermaymon <omermaymon2110@gmail.com>
2021-06-09 11:55:38 +0300

    "fixed" inheritence issuse in course_page.dart

--- lib/screens/course_page.dart
+++ lib/screens/course_page.dart
@@ -25,6 +25,14 @@ class CoursePage extends StatefulWidget {
 class _CoursePageState extends State<CoursePage> {
   final messageController = TextEditingController();
   final _firebaseauth_instance = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    messageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -81,45 +89,51 @@ class _CoursePageState extends State<CoursePage> {
                                 'השאר ביקורת',
                                 textDirection: TextDirection.rtl,
                               ),
-                              content: Column(
-                                children: [
-                                  TextFormField(
-                                    validator: (value) {
-                                      if (value == null || value.isEmpty) {
-                                        return 'Please enter some text';
-                                      }
-                                      return null;
-                                    },
-                                    controller: messageController,
-                                    decoration: const InputDecoration(
-                                      icon: const Icon(Icons.question_answer),
-                                      hintText: '? מה אומר',
-                                      labelText: 'הביקורת שלך כאן',
+                              content: Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: [
+                                    TextFormField(
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please enter some text';
+                                        }
+                                        return null;
+                                      },
+                                      controller: messageController,
+                                      decoration: const InputDecoration(
+                                        icon: const Icon(Icons.question_answer),
+                                        hintText: '? מה אומר',
+                                        labelText: 'הביקורת שלך כאן',
+                                      ),
+                                      maxLines: null,
                                     ),
-                                    maxLines: null,
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.only(
-                                        left: 150.0, top: 40.0),
-                                    child: new ElevatedButton(
-                                        onPressed: () {
-                                          FocusScope.of(context).unfocus();
-                                          // Validate returns true if the form is valid, or false otherwise.
-                                          // If the form is valid, display a snackbar. In the real world,
-                                          // you'd often call a server or save the information in a database.
-                                          ScaffoldMessenger.of(context)
-                                              .showSnackBar(SnackBar(
-                                                  content: Text(
-                                            'מעלה ביקורת...',
-                                            textDirection: TextDirection.rtl,
-                                          )));
-                                          FirebaseFirestore.instance
-                                              .collection("Reviews")
-                                              .doc(widget.course_number +
-                                                  " " +
-                                                  _firebaseauth_instance
-                                                      .currentUser.displayName)
-                                              .set({
+                                    Container(
+                                      padding: const EdgeInsets.only(
+                                          left: 150.0, top: 40.0),
+                                      child: new ElevatedButton(
+                                          onPressed: () {
+                                            FocusScope.of(context).unfocus();
+                                            // Validate returns true if the form is valid, or false otherwise.
+                                            // If the form is valid, display a snackbar. In the real world,
+                                            // you'd often call a server or save the information in a database.
+                                            if (_formKey.currentState
+                                                .validate()) {
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(SnackBar(
+                                                      content: Text(
+                                                'מעלה ביקורת...',
+                                                textDirection:
+                                                    TextDirection.rtl,
+                                              )));
+                                              FirebaseFirestore.instance
+                                                  .collection("Reviews")
+                                                  .doc(widget.course_number +
+                                                      " " +
+                                                      _firebaseauth_instance
+                                                          .currentUser
+                                                          .displayName)
+                                                  .set({
                                                 'time':
                                                     DateTime.now().toString(),
                                                 'name': _firebaseauth_instance
@@ -135,29 +149,29 @@ class _CoursePageState extends State<CoursePage> {
                                                 'user_photo':
                                                     _firebaseauth_instance
                                                         .currentUser.photoURL,
-                                              })
-                                              .then((_) =>
-                                                  ScaffoldMessenger.of(context)
-                                                      .showSnackBar(SnackBar(
-                                                          content: Text(
-                                                    'סחתיין עליך !',
-                                                    textDirection:
-                                                        TextDirection.rtl,
-                                                  ))))
-                                              .catchError((_) => ScaffoldMessenger
-                                                      .of(context)
-                                                  .showSnackBar(SnackBar(
-                                                      content: Text(
-                                                          'Ya Walli, we have a problem.'))));
+                                              }).then((_) =>
+                                                      ScaffoldMessenger.of(
+                                                              context)
+                                                          .showSnackBar(
+                                                              SnackBar(
+                                                                  content: Text(
+                                                        'סחתיין עליך !',
+                                                        textDirection:
+                                                            TextDirection //TODO WE REMOVED CATCH ERROR FROM HERE
+                                                                .rtl,
+                                                      ))));
 
-                                          int count = 0;
-                                          Navigator.popUntil(context, (route) {
-                                            return count++ == 3;
-                                          });
-                                        },
-                                        child: const Text('שלחחחח')),
-                                  ),
-                                ],
+                                              int count = 0;
+                                              Navigator.popUntil(context,
+                                                  (route) {
+                                                return count++ == 3;
+                                              });
+                                            }
+                                          },
+                                          child: const Text('שלחחחח')),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ));
                   },
